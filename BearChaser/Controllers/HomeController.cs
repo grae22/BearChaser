187cd160d684ad2da72f04814d2175b7fb35d24f
GraemeB <graeme.bruschi@derivco.co.za>
2018-01-18 10:31:39 +0000

    More cleanup of unnecessary files.

--- BearChaser/Controllers/HomeController.cs
+++ BearChaser/Controllers/HomeController.cs
@@ -4,11 +4,15 @@ namespace BearChaser.Controllers
 {
   public class HomeController : Controller
   {
+    //---------------------------------------------------------------------------------------------
+
     public ActionResult Index()
     {
-      ViewBag.Title = "Home Page";
+      ViewBag.Title = "BearChaserAPI";
 
       return View();
     }
+
+    //---------------------------------------------------------------------------------------------
   }
 }
